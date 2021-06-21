f850ff96c22ac1a942d05b863cd325822be44e12
Andrew Lam <lekz01@gmail.com>
2021-06-21 17:12:38 -0400

    Chapter 03 - Exercises

---
+++ Chapter03/Exercise02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            int max = 500;
+            try { // Add this to branch in case of overflow
+                checked { // Add this to prevent overflow-related infinite for-loop
+                    for (byte i = 0; i < max; i++) {
+                        WriteLine(i);
+                    }
+                }
+            } catch (OverflowException oe) {
+                WriteLine($"{oe.Message}");
+            }
+        }
+    }
+}
---
+++ Chapter03/Exercise04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise04 {
+    class Program {
+        static void Main(string[] args) {
+            byte firstInput;
+            byte secondInput;
+            try {
+                Write("Enter a number between 0 and 255: ");
+                firstInput = byte.Parse(ReadLine());
+                Write("Enter another number between 0 and 255: ");
+                secondInput = byte.Parse(ReadLine());
+                WriteLine($"{firstInput} divided by {secondInput} is {firstInput / secondInput}");
+            } catch (FormatException fe) {
+                WriteLine($"{fe.Message}");
+            } catch {
+                WriteLine("An input is out of range.");
+            }
+        }
+    }
+}
