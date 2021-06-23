55126e8fcf0f79dc4000bb792a3d92f1001d9fdc
Andrew Lam <lekz01@gmail.com>
2021-06-23 16:49:33 -0400

    Chapter 04 - Exercise 4.2 polishing console program

--- Chapter04/PrimeFactors/PrimeFactorsCalculator/Program.cs
+++ Chapter04/PrimeFactors/PrimeFactorsCalculator/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using PrimeFactorsLib;
 using System.Collections.Generic;
+using static System.Console;
 
 namespace PrimeFactorsCalculator {
     class Program {
         static void Main(string[] args) {
-            int input = 120;
-            Console.WriteLine($"Prime factors of {input} are: ");
-            var pfCalc = new PrimeFactors();
-            List<int> pfResults = pfCalc.PrimeFactorsCalc(input);
-            foreach (int num in pfResults) {
-                Console.Write($"{num} ");
+            Write("Which number do you want to prime factorize? ");
+            try {
+                int input = int.Parse(ReadLine());
+                WriteLine($"Prime factors of {input} are: ");
+                var pfCalc = new PrimeFactors();
+                List<int> pfResults = pfCalc.PrimeFactorsCalc(input);
+                foreach (int num in pfResults) {
+                    Write($"{num} ");
+                }
+            } catch (FormatException) {
+                WriteLine("The input is not a valid format.");
+            } catch (OverflowException) {
+                WriteLine("The input value is too big.");
+            } catch (Exception e) {
+                WriteLine($"{e.GetType()} says {e.Message}");
             }
         }
     }
