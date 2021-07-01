77a84f55c3e87c1b3092cea80f9f3a3d0c2ecb97
Andrew Lam <lekz01@gmail.com>
2021-07-01 16:39:04 -0400

    Chapter 06 - Implementing class functionality with methods.

---
+++ Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace Packt.Shared {
+    public class Person {
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+
+        public void WriteToConsole() {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+
+        // Static Method - an action this object type does
+        public static Person Procreate(Person p1, Person p2) {
+            var baby = new Person {
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+            return baby;
+        }
+
+        // Instance Method - actions object does on itself
+        public Person ProcreateWith(Person partner) {
+            return Procreate(this, partner);
+        }
+    }
+}
---
+++ Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Packt.Shared;
+using static System.Console;
+
+namespace PeopleApp {
+    class Program {
+        static void Main(string[] args) {
+            Person harry = new Person { Name = "Harry" };
+            var mary = new Person { Name = "Mary"};
+            Person jill = new Person { Name = "Jill" };
+
+            // instance method call
+            var baby1 = mary.ProcreateWith(harry);
+            baby1.Name = "Gary";
+
+            // static method call
+            var baby2 = Person.Procreate(harry, jill);
+            WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+            WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+            WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+            WriteLine("{0}'s first child is named \"{1}\".",
+                      harry.Name,
+                      harry.Children[0].Name);
+        }
+    }
+}
