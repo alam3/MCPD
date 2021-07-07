a1f9a6b374f9cca1769cd47a02d91a797903a2db
Andrew Lam <lekz01@gmail.com>
2021-07-07 19:04:49 -0400

    Chapter - Hiding members in parent class.

--- Chapter06/PacktLibrary/Employee.cs
+++ Chapter06/PacktLibrary/Employee.cs
@@ -1,4 +1,5 @@
 using System;
+using static System.Console;
 
 namespace Packt.Shared {
     // Inheriting from another class (Person)
@@ -6,5 +7,14 @@ namespace Packt.Shared {
     public class Employee : Person {
         public string EmployeeCode { get; set; }
         public DateTime HireDate { get; set; }
+
+        // Hiding inherited members, in this case 'WriteToConsole()'
+        // Note how this method is called instead of the one in 'Person'
+        public void WriteToConsole() {
+            WriteLine("{0} was born on {1:dd/MM/yy} and hired on {2:dd/MM/yy}",
+                      arg0: Name,
+                      arg1: DateOfBirth,
+                      arg2: HireDate);
+        }
     }
 }
