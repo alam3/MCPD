de730af29b141576ad36586b8f0758f606f18d7a
Andrew Lam <lekz01@gmail.com>
2021-07-08 19:18:27 -0400

    Chapter 06 - Practice Exercise pt. 2

--- Chapter06/Exercise02/Circle.cs
+++ Chapter06/Exercise02/Circle.cs
@@ -2,8 +2,8 @@ using System;
 
 public class Circle : Shape {
     public Circle(double diameter) {
-        Height = diameter;
-        Width = diameter;
+        Height = diameter * 2;
+        Width = diameter * 2;
         Area = Math.PI * Math.Pow(diameter, 2);
     }
 }
