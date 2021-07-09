5f3aa9fe0368c0f0694da9043999bdba3396b04c
Andrew Lam <lekz01@gmail.com>
2021-07-08 21:04:58 -0400

    Chapter 07 - Namespaces and Assemblies.

---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Xml.Linq; // Needs to be added for 'XDocument()' to work,
+                       // even if the project has the the reference to the assembly
+                       // that contains the type. The compiler doesn't know what
+                       // namespace the method belongs to.
+
+namespace AssembliesAndNamespaces {
+    class Program {
+        static void Main(string[] args) {
+            var doc = new XDocument();
+        }
+    }
+}
