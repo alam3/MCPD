5d9e3204d11d61ebb6da46ef73561f5d2e0ab342
Andrew Lam <lekz01@gmail.com>
2021-07-08 21:13:53 -0400

    Chapter 07 - C# keyword types vs Namespace type differences

--- Chapter07/AssembliesAndNamespaces/Program.cs
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 using System.Xml.Linq; // Needs to be added for 'XDocument()' to work,
                        // even if the project has the the reference to the assembly
                        // that contains the type. The compiler doesn't know what
@@ -7,7 +8,14 @@ using System.Xml.Linq; // Needs to be added for 'XDocument()' to work,
 namespace AssembliesAndNamespaces {
     class Program {
         static void Main(string[] args) {
+            // Needs namespace import to work
             var doc = new XDocument();
+
+            // C# type keywords as aliases of .NET types in a class library assembly
+            string s1 = "Hello";
+            String s2 = "World"; // Causes compiler error if 'using System;' is removed.
+            WriteLine($"{s1} {s2}");
+
         }
     }
 }
