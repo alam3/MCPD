707a1d00bb233fcb0b09c1aef9597f8560de7023
Andrew Lam <lekz01@gmail.com>
2021-07-13 17:46:58 -0400

    Chapter 08 - .NET strings, characters, and string manipulation.

---
+++ Chapter08/WorkingWithText/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace WorkingWithText {
+    class Program {
+        static void Main(string[] args) {
+
+            // Working with Text types - StringBuilder and Regex help with manipulating strings
+            string city = "London";
+            WriteLine($"{city} is {city.Length} characters long.");
+
+            // 'string' class stores text in an array of 'char'
+            WriteLine($"First char is {city[0]} and third is {city[3]}.");
+        }
+    }
+}
