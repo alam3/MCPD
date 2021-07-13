451a3e92e40a38e27f0c5020017ed095458ea6e7
Andrew Lam <lekz01@gmail.com>
2021-07-13 17:40:26 -0400

    Chapter 08 - .NET types: Big integers and complex numbers.

---
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+using System.Numerics;
+
+namespace WorkingWithNumbers {
+    class Program {
+        static void Main(string[] args) {
+            
+            // Testing .NET Types - big numbers
+            var largest = ulong.MaxValue;
+            WriteLine($"{largest,40:N0}");
+            var atomsInTheUniverse = BigInteger.Parse("123456789012345678901234567890");
+            WriteLine($"{atomsInTheUniverse,40:N0}");
+
+            // C# working Complex numbers
+            var c1 = new Complex(4, 2);
+            var c2 = new Complex(3, 7);
+            var c3 = c1 + c2;
+            WriteLine($"{c1} added to {c2} is {c3}");
+        }
+    }
+}
