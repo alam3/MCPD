e764ecae606efd6b6a44564b4d83b01b9184b52e
Andrew Lam <lekz01@gmail.com>
2021-07-12 21:58:28 -0400

    Chapter 07 - Testing the created NuGet package.

--- Chapter07/AssembliesAndNamespaces/Program.cs
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -4,6 +4,7 @@ using System.Xml.Linq; // Needs to be added for 'XDocument()' to work,
                        // even if the project has the the reference to the assembly
                        // that contains the type. The compiler doesn't know what
                        // namespace the method belongs to.
+using Packt.Shared; // Import Packt.Shared namespace
 
 namespace AssembliesAndNamespaces {
     class Program {
@@ -15,6 +16,18 @@ namespace AssembliesAndNamespaces {
             string s1 = "Hello";
             String s2 = "World"; // Causes compiler error if 'using System;' is removed.
             WriteLine($"{s1} {s2}");
+
+            // Testing packaged library in NuGet (Packt.CSdotnet.SharedLibrary)
+            Write("Enter a color value in hex: ");
+            string hex = ReadLine();
+            WriteLine("Is {0} a valid color value? {1}", arg0: hex, arg1: hex.IsValidHex());
+
+            Write("Enter a XML element: ");
+            string xmlTag = ReadLine();
+            WriteLine("Is {0} a valid XML element? {1}", arg0: xmlTag, arg1: xmlTag.IsValidXmlTag());
+            Write("Enter a password: ");
+            string password = ReadLine();
+            WriteLine("Is {0} a valid password? {1}", arg0: password, arg1: password.IsValidPassword());
 
         }
     }
