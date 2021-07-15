c2f247ff584e95289aa519b9de8c76d87a89c3f7
Andrew Lam <lekz01@gmail.com>
2021-07-15 00:09:04 -0400

    Chapter 08 - Using Regular Expressions, continued

--- Chapter08/WorkingWithRegularExpressions/Program.cs
+++ Chapter08/WorkingWithRegularExpressions/Program.cs
@@ -7,7 +7,12 @@ namespace WorkingWithRegularExpressions {
         static void Main(string[] args) {
             Write("Enter your age: ");
             string input = ReadLine();
-            var ageChecker = new Regex(@"\d"); // Form Regex to check that input is a digit
+            var ageChecker = new Regex(@"^\d+$"); // Form Regex to check that input is a digit
+            // '@' switches off the use of escape characters, making '\d' interpretable as a RegEx
+            // '^' means to match the starting position of a string, 
+            // making it so that it will only accept a string starting with a digit
+            // '$' matches the end position of a string. In the case '^\d$', only one digit is detected
+            // '+' makes it so there is one or more of the preceeding expression ('\d' in this case)
             if (ageChecker.IsMatch(input)) {
                 WriteLine("Thank you!");
             } else {
