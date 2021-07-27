e2984b6d726c07a58144b1c47913bb15cbe466d8
Andrew Lam <lekz01@gmail.com>
2021-07-26 23:23:22 -0400

    Chapter 08 - Creating a custom attribute. Querying the Custom Attribute.

--- Chapter08/WorkingWithReflection/Program.cs
+++ Chapter08/WorkingWithReflection/Program.cs
@@ -24,12 +24,34 @@ namespace WorkingWithReflection {
             WriteLine($" Version: {version.InformationalVersion}");
             var company = assembly.GetCustomAttribute<AssemblyCompanyAttribute>();
             WriteLine($" Company: {company.Company}");
+
+            // Get types, enumerate members, and read custom attribute 'Coder' from them
+            WriteLine();
+            WriteLine($"* Types:");
+            Type[] types = assembly.GetTypes();
+            foreach (Type type in types) {
+                WriteLine();
+                WriteLine($"Type: {type.FullName}");
+                MemberInfo[] members = type.GetMembers();
+                foreach (MemberInfo member in members) {
+                    WriteLine("{0}: {1} ({2}))",
+                              arg0: member.MemberType,
+                              arg1: member.Name,
+                              arg2: member.DeclaringType);
+                    var coders = member.GetCustomAttributes<CoderAttribute>().OrderByDescending(c => c.LastModified);
+                    foreach (CoderAttribute coder in coders) {
+                        WriteLine("-> Modified by {0} on {1}",
+                                  coder.Coder,
+                                  coder.LastModified.ToShortDateString());
+                    }
+                }
+            }
         }
 
         [Coder("Mark Price", "22 August 2019")]
         [Coder("Johnni Rasmussen", "13 September 2019")]
         public static void DoStuff() {
-            
+
         }
     }
 }
