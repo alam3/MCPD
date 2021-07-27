4d15dc0767c7d39977a5e1d9193f48d553344aa1
Andrew Lam <lekz01@gmail.com>
2021-07-27 12:53:02 -0400

    Chapter 08 - Exercise02 Regex Practice

---
+++ Chapter08/Exercise02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+using System.Text.RegularExpressions;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            ConsoleKeyInfo endkey;
+            do {
+                DoRegex();
+                endkey = ReadKey();
+            } while (endkey.Key != ConsoleKey.Escape);
+        }
+
+        public static void DoRegex() {
+            Write("\nEnter a regular expression (or press ENTER to use the default): ");
+            string regex = ReadLine();
+            Write("Enter some input: ");
+            string matchInput = ReadLine();
+            Regex inputChecker = new Regex(regex);
+            WriteLine($"{matchInput} matches {regex}? {inputChecker.IsMatch(matchInput)}");
+            WriteLine("Press ESC to end or any key to try again.");
+        }
+    }
+}
