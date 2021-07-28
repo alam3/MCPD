c5f54c2744e2315a8988756d6e2d1175a2d2bade
Andrew Lam <lekz01@gmail.com>
2021-07-27 23:03:20 -0400

    Chapter 09 - Working with FileSystems. Identifying path differences for different OSes

---
+++ Chapter09/WorkingWithFileSystems/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+namespace WorkingWithFileSystems {
+    class Program {
+        static void Main(string[] args) {
+            OutputFileSystemInfo();
+        }
+
+        // Look at how paths are different depending on the Operating System
+        static void OutputFileSystemInfo() {
+            WriteLine("{0,-33} {1}", "Path.PathSeparator", PathSeparator);
+            WriteLine("{0,-33} {1}", "Path.DirectorySeparatorChar", DirectorySeparatorChar);
+            WriteLine("{0,-33} {1}", "Directory.GetCurrentDirectory()", GetCurrentDirectory());
+            WriteLine("{0,-33} {1}", "Environment.SystemDirectory", SystemDirectory);
+            WriteLine("{0,-33} {1}", "Path.GetTempPath()", GetTempPath());
+            WriteLine("GetFolderPath(SpecialFolder");
+            WriteLine("{0,-33} {1}", ".System)", GetFolderPath(SpecialFolder.System));
+            WriteLine("{0,-33} {1}", ".ApplicationData)", GetFolderPath(SpecialFolder.ApplicationData));
+            WriteLine("{0,-33} {1}", ".MyDocuments)", GetFolderPath(SpecialFolder.MyDocuments));
+            WriteLine("{0,-33} {1}", ".Personal)", GetFolderPath(SpecialFolder.Personal));
+        }
+    }
+}
