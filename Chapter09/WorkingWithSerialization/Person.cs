273678f76ce564421bdfd1b59235efe4f64cdcd2
Andrew Lam <lekz01@gmail.com>
2021-08-04 15:40:13 -0400

    Chapter 09 - Serializing XML with Compact XML using the XmlAttribute attribute

--- Chapter09/WorkingWithSerialization/Person.cs
+++ Chapter09/WorkingWithSerialization/Person.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Xml.Serialization; // To generate compact XML, e.g. setting a shortname
 
 namespace Packt.Shared {
     public class Person {
@@ -10,8 +11,11 @@ namespace Packt.Shared {
             Salary = initialSalary;
         }
 
+        [XmlAttribute("fname")] // For compact XML
         public string FirstName { get; set; }
+        [XmlAttribute("lname")] // For compact XML
         public string LastName { get; set; }
+        [XmlAttribute("dob")] // For compact XML
         public DateTime DateOfBirth { get; set; }
         public HashSet<Person> Children { get; set; }
         protected decimal Salary { get; set; }
