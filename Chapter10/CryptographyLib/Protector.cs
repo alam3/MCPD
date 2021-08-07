151638c9e7fee9d52a23170acd44c68871d97c1f
Andrew Lam <lekz01@gmail.com>
2021-08-07 10:49:37 -0400

    Chapter 10 - Salting and hashing. Example of user registration

--- Chapter10/CryptographyLib/Protector.cs
+++ Chapter10/CryptographyLib/Protector.cs
@@ -47,5 +47,40 @@ namespace Packt.Shared {
             return Encoding.Unicode.GetString(plainBytes);
         }
 
+        // Hashing with SHA256
+        private static Dictionary<string, User> Users = new Dictionary<string, User>();
+        public static User Register(string username, string password) {
+            // generate a random salt
+            var rng = RandomNumberGenerator.Create();
+            var saltBytes = new byte[16];
+            rng.GetBytes(saltBytes);
+            var saltText = Convert.ToBase64String(saltBytes);
+
+            // generate the salted and hashed password
+            var saltedhashedPassword = SaltAndHashPassword(password, saltText);
+            var user = new User {
+                Name = username, Salt = saltText, SaltedHashedPassword = saltedhashedPassword
+            };
+            Users.Add(user.Name, user);
+            return user;
+        }
+
+        public static bool CheckPassword(string username, string password) {
+            if (!Users.ContainsKey(username)) { 
+                return false;
+            }
+            var user = Users[username];
+
+            // re-generate the salted and hashed password 
+            var saltedhashedPassword = SaltAndHashPassword(password, user.Salt);
+            return (saltedhashedPassword == user.SaltedHashedPassword);
+        }
+
+        public static string SaltAndHashPassword(string password, string salt) {
+            var sha = SHA256.Create();
+            var saltedPassword = password + salt;
+            return Convert.ToBase64String(sha.ComputeHash(Encoding.Unicode.GetBytes(saltedPassword)));
+        }
+
     }
 }
