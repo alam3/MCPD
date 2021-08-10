5e34767630252e548e432537ffad5d64280df897
Andrew Lam <lekz01@gmail.com>
2021-08-10 16:58:26 -0400

    Chapter 10 - Exercise 02, forming XML serialization

---
+++ Chapter10/Exercise02/Customer.cs
@@ -0,0 +1,12 @@
+using System;
+// using System.Xml.Serialization;
+
+namespace Packt.Shared {
+    public class Customer {
+        public Customer() { }
+
+        public string Name { get; set; }
+        public string CreditCard { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Chapter10/Exercise02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+using Packt.Shared; // Customer
+using System.Collections.Generic; // List<T>
+using System.Xml.Serialization; // XmlSerializer
+using System.IO; // Filestream
+using static System.Environment;
+using static System.IO.Path;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var customers = new List<Customer>();
+
+            WriteLine("Enter customer name: ");
+            string customerName = ReadLine();
+            WriteLine("Enter customer credit card: ");
+            string customerCard = ReadLine();
+            WriteLine("Enter customer password: ");
+            string customerPassword = ReadLine();
+
+            customers.Add(new Customer { Name = customerName,
+                                         CreditCard = customerCard,
+                                         Password = customerPassword });
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Customer>));
+            string savePath = ("customers.xml");
+            using (FileStream fileStream = File.Create(savePath)) {
+                xmlSerializer.Serialize(fileStream, customers);
+            }
+        }
+    }
+}
