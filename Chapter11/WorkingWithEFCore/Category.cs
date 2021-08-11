0daa4c96bec84cde5bff49fa3d1d4534498db34a
Andrew Lam <lekz01@gmail.com>
2021-08-11 17:05:01 -0400

    Chapter 11 - Setting up classes to match DB tables

---
+++ Chapter11/WorkingWithEFCore/Category.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Packt.Shared {
+    public class Category {
+        // Properties mapping to the columns for 'Category' in database
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        [Column(TypeName = "ntext")]
+        public string Description { get; set; }
+
+        // defines a navigation property for related rows
+        public virtual ICollection<Product> Products { get; set; }
+        public Category() {
+            // to enable developers to add products to a Category we must initialize the navigation property to an empty collection
+            this.Products = new HashSet<Product>();
+        }
+    }
+}
