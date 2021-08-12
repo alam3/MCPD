4613cd754ea965f72ea263837625f7575ab30f37
Andrew Lam <lekz01@gmail.com>
2021-08-11 23:24:32 -0400

    Chapter 11 - Setting up Northwind class to define the database

--- Chapter11/WorkingWithEFCore/Northwind.cs
+++ Chapter11/WorkingWithEFCore/Northwind.cs
@@ -1,5 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Packt.Shared {
-    public class Northwind {
-        
+    public class Northwind : DbContext {
+        // Northwind class represents the database
+        // Using EF Core requires inheriting from DbContext, which translates/generates SQL statements from C# code
+
+        // these properties, using DbSet<t>, map to tables in the database
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        // Use OnConfiguring to set the database connection string
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "Northwind.db");
+            optionsBuilder.UseSqlite($"Filename={path}");
+        }
+
+        // Required overridden method for writing Fluent API statements
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            // example of using Fluent API instead of attributes to limit the length of a category name to 15
+            modelBuilder.Entity<Category>()
+                .Property(category => category.CategoryName)
+                .IsRequired() // NOT NULL
+                .HasMaxLength(15);
+            // added to "fix" the lack of decimal support in SQLite
+            modelBuilder.Entity<Product>()
+                .Property(product => product.Cost)
+                .HasConversion<double>();
+        }
+
     }
 }
