7beedae20e6ae9b7d388d47e6ce3406d4ede3bf5
Andrew Lam <lekz01@gmail.com>
2021-08-17 15:51:07 -0400

    Chapter 11 - LINQ data manipulation

---
+++ Chapter12/LinqWithObjects/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+using System.Linq; // Requires to use the extension methods for array/collection types
+
+namespace LinqWithObjects {
+    class Program {
+        static void Main(string[] args) {
+            LinqWithArrayOfStrings();
+        }
+
+        static void LinqWithArrayOfStrings() {
+            var names = new string[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed"};
+            var query = names.Where(new Func<string, bool>(NameLongerThanFour)); // Delegate needs to be passed the string of the method to use
+            foreach (string item in query) {
+                WriteLine(item);
+            }
+        }
+
+        static bool NameLongerThanFour(string name) {
+            return name.Length > 4;
+        }
+
+    }
+}
