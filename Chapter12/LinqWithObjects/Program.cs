2e9c8de945b9766efa16e10763d341c9e3b2ecaf
Andrew Lam <lekz01@gmail.com>
2021-08-17 16:36:03 -0400

    Chapter 11 - LINQ data manipulation - implicit delegate instantiation

--- Chapter12/LinqWithObjects/Program.cs
+++ Chapter12/LinqWithObjects/Program.cs
@@ -10,7 +10,8 @@ namespace LinqWithObjects {
 
         static void LinqWithArrayOfStrings() {
             var names = new string[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed"};
-            var query = names.Where(new Func<string, bool>(NameLongerThanFour)); // Delegate needs to be passed the string of the method to use
+            // var query = names.Where(new Func<string, bool>(NameLongerThanFour)); // Delegate needs to be passed the string of the method to use
+            var query = names.Where(NameLongerThanFour); // Simplified where the explicit instantiation of the delegate is removed
             foreach (string item in query) {
                 WriteLine(item);
             }
