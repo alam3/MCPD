0e78163c265350c6a59c1d29bae71647c83ef7d7
Andrew Lam <lekz01@gmail.com>
2021-08-19 17:50:32 -0400

    Chapter 12 - LINQ with EF Core - Projection instances of a type to a new one with fewer properties

--- Chapter12/LinqWithEFCore/Program.cs
+++ Chapter12/LinqWithEFCore/Program.cs
@@ -16,7 +16,17 @@ namespace LinqWithEFCore {
                     // query is a DbSet<Product>
                     .Where(product => product.UnitPrice < 10M)
                     // query is now an IQueryable<Product>
-                    .OrderByDescending(product => product.UnitPrice);
+                    .OrderByDescending(product => product.UnitPrice)
+                    // query is now an IOrderedQueryable<Product>
+                    // Section: Projecting sequences into new types. In this case SELECT allows for a more efficient call
+                    // to the 3 columns needed rather than all the columns
+                    .Select(product => new { // anonymous type
+                        product.ProductID,
+                        product.ProductName,
+                        product.UnitPrice
+                    });
+
+
                 WriteLine("Products that cost less than $10:");
                 foreach (var item in query) {
                     WriteLine("{0}: {1} costs {2:$#,##0.00}",
