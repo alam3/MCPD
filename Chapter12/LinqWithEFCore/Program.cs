b7066b18979512e99abad229eeb0b2cc59942031
Andrew Lam <lekz01@gmail.com>
2021-08-19 23:21:48 -0400

    Chapter 12 - LINQ with EF Core - Joining and grouping

--- Chapter12/LinqWithEFCore/Program.cs
+++ Chapter12/LinqWithEFCore/Program.cs
@@ -7,7 +7,8 @@ using System.Linq;
 namespace LinqWithEFCore {
     class Program {
         static void Main(string[] args) {
-            FilterAndSort();
+            // FilterAndSort();
+            JoinCategoriesAndProducts();
         }
 
         static void FilterAndSort() {
@@ -26,13 +27,30 @@ namespace LinqWithEFCore {
                         product.UnitPrice
                     });
 
-
                 WriteLine("Products that cost less than $10:");
                 foreach (var item in query) {
                     WriteLine("{0}: {1} costs {2:$#,##0.00}",
                     item.ProductID, item.ProductName, item.UnitPrice);
                 }
                 WriteLine();
+            }
+        }
+
+        static void JoinCategoriesAndProducts() {
+            using (var db = new Northwind()) {
+                // join every product to its category to return 77 matches
+                var queryJoin = db.Categories.Join(
+                    inner: db.Products,
+                    outerKeySelector: category => category.CategoryID,
+                    innerKeySelector: product => product.CategoryID,
+                    resultSelector: (c, p) => new { c.CategoryName, p.ProductName, p.ProductID })
+                    .OrderBy(cp => cp.CategoryName);
+                foreach (var item in queryJoin) {
+                    WriteLine("{0}: {1} is in {2}.",
+                        arg0: item.ProductID,
+                        arg1: item.ProductName,
+                        arg2: item.CategoryName);
+                }
             }
         }
 
