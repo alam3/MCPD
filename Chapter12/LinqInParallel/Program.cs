e8cc3a20e593c681bda49c4e4514fba1f8e91dbb
Andrew Lam <lekz01@gmail.com>
2021-08-23 16:14:26 -0400

    Chapter 12 - LINQ with EF Core - Parallelization with AsParallel()

---
+++ Chapter12/LinqInParallel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqInParallel {
+    class Program {
+        static void Main(string[] args) {
+            // Multi-threading
+            // First, check performance of creating and squaring 2 billion integers without parallelization
+            var watch = new Stopwatch();
+            Write("Press ENTER to start: ");
+            ReadLine();
+            watch.Start();
+            IEnumerable<int> numbers = Enumerable.Range(1, 2_000_000_000);
+            // var squares = numbers.Select(number => number * number).ToArray(); // Not parallelized
+            var squares = numbers.AsParallel()
+                                 .Select(number => number * number).ToArray(); // Parallelized
+            watch.Stop();
+            WriteLine("{0:#,##0} elapsed milliseconds.", watch.ElapsedMilliseconds);
+        }
+    }
+}
