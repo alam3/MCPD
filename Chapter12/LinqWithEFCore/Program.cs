9ef32c60671ed930c2a6844bd795b4c899c54c76
Andrew Lam <lekz01@gmail.com>
2021-08-23 16:53:30 -0400

    Chapter 12 - LINQ with EF Core - Custom chainable LINQ extension method

--- Chapter12/LinqWithEFCore/Program.cs
+++ Chapter12/LinqWithEFCore/Program.cs
@@ -7,16 +7,42 @@ using System.Linq;
 namespace LinqWithEFCore {
     class Program {
         static void Main(string[] args) {
-            // FilterAndSort();
+            FilterAndSort();
             // JoinCategoriesAndProducts();
             // GroupJoinCategoriesAndProducts();
-            AggregateProducts();
+            // AggregateProducts();
         }
 
-        static void FilterAndSort() {
+        // static void FilterAndSort() { // Original method
+        //     using (var db = new Northwind()) {
+        //         var query = db.Products
+        //             // query is a DbSet<Product>
+        //             .Where(product => product.UnitPrice < 10M)
+        //             // query is now an IQueryable<Product>
+        //             .OrderByDescending(product => product.UnitPrice)
+        //             // query is now an IOrderedQueryable<Product>
+        //             // Section: Projecting sequences into new types. In this case SELECT allows for a more efficient call
+        //             // to the 3 columns needed rather than all the columns
+        //             .Select(product => new { // anonymous type
+        //                 product.ProductID,
+        //                 product.ProductName,
+        //                 product.UnitPrice
+        //             });
+
+        //         WriteLine("Products that cost less than $10:");
+        //         foreach (var item in query) {
+        //             WriteLine("{0}: {1} costs {2:$#,##0.00}",
+        //             item.ProductID, item.ProductName, item.UnitPrice);
+        //         }
+        //         WriteLine();
+        //     }
+        // }
+
+        static void FilterAndSort() { // Method using custom chainable LINQ extension method
             using (var db = new Northwind()) {
                 var query = db.Products
                     // query is a DbSet<Product>
+                    .ProcessSequence()
                     .Where(product => product.UnitPrice < 10M)
                     // query is now an IQueryable<Product>
                     .OrderByDescending(product => product.UnitPrice)
