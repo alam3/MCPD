48f13f6448339937e523b92861a991f9d49cabe6
Andrew Lam <lekz01@gmail.com>
2021-08-24 15:41:25 -0400

    Chapter 13 - Implementing a monitoring class

---
+++ Chapter13/MonitoringApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+using System.Linq;
+using Packt.Shared;
+
+namespace MonitoringApp {
+    class Program {
+        static void Main(string[] args) {
+            WriteLine("Processing. Please wait...");
+            Recorder.Start();
+            // simulate a process that requires memory resources...
+            int[] largeArrayOfInts = Enumerable.Range(1, 10_000).ToArray();
+            // ... and takes some time to complete
+            System.Threading.Thread.Sleep(new Random().Next(5, 10) * 1000);
+            Recorder.Stop();
+        }
+    }
+}
---
+++ Chapter13/MonitoringLib/Recorder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+using System.Diagnostics;
+using static System.Diagnostics.Process;
+
+namespace Packt.Shared {
+    public static class Recorder {
+        static Stopwatch timer = new Stopwatch();
+        static long bytesPhysicalBefore = 0;
+        static long bytesVirtualBefore = 0;
+
+        public static void Start() {
+            // force two garbage collections to release memory that is
+            // no longer referenced but has not been released yet.
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            // store the current physical and virtual memory use
+            bytesPhysicalBefore = GetCurrentProcess().WorkingSet64;
+            bytesVirtualBefore = GetCurrentProcess().VirtualMemorySize64;
+            timer.Restart();
+        }
+
+        public static void Stop() {
+            timer.Stop();
+            long bytesPhysicalAfter = GetCurrentProcess().WorkingSet64;
+            long bytesVirtualAfter = GetCurrentProcess().VirtualMemorySize64;
+            WriteLine($"{bytesPhysicalAfter - bytesPhysicalBefore:N0} physical bytes used.");
+            WriteLine($"{bytesVirtualAfter - bytesVirtualBefore:N0} virtual bytes used.");
+            WriteLine($"{timer.Elapsed} time span elapsed.");
+            WriteLine($"{timer.ElapsedMilliseconds:N0} total milliseconds elapsed.");
+        }
+    }
+}
