5e7c982b3b125eeacf37473136e1f3ce19203114
Andrew Lam <lekz01@gmail.com>
2021-08-24 15:47:59 -0400

    Chapter 13 - Measuring efficiency of processing strings. String concatenation (+) vs. StringBuilder()

--- Chapter13/MonitoringApp/Program.cs
+++ Chapter13/MonitoringApp/Program.cs
@@ -6,6 +6,9 @@ using Packt.Shared;
 namespace MonitoringApp {
     class Program {
         static void Main(string[] args) {
+
+            // Testing the Recorder class monitors 
+            /*
             WriteLine("Processing. Please wait...");
             Recorder.Start();
             // simulate a process that requires memory resources...
@@ -13,6 +16,27 @@ namespace MonitoringApp {
             // ... and takes some time to complete
             System.Threading.Thread.Sleep(new Random().Next(5, 10) * 1000);
             Recorder.Stop();
+            */
+
+            // Measuring the efficiency of processing strings
+            int[] numbers = Enumerable.Range(1, 50_000).ToArray();
+            WriteLine("Using string with +");
+            Recorder.Start();
+            string s = "";
+            for (int i = 0; i < numbers.Length; i++) {
+                s += numbers[i] + ", ";
+            }
+            Recorder.Stop();
+
+            WriteLine("Using StringBuilder");
+            Recorder.Start();
+            var builder = new System.Text.StringBuilder();
+            for (int i = 0; i < numbers.Length; i++) {
+                builder.Append(numbers[i]);
+                builder.Append(", ");
+            }
+            Recorder.Stop();
+
         }
     }
 }
