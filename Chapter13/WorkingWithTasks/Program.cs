c2a6f2d7e371e4fd70a0a1b6020ad87ea2cfd8b6
Andrew Lam <lekz01@gmail.com>
2021-08-24 16:09:43 -0400

    Chapter 13 - Running multiple methods - Synchronously

---
+++ Chapter13/WorkingWithTasks/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace WorkingWithTasks {
+    class Program {
+        static void Main(string[] args) {
+
+            // Running the methods synchronously
+            var timer = Stopwatch.StartNew();
+            WriteLine("Running methods synchronously on one thread.");
+            MethodA();
+            MethodB();
+            MethodC();
+            WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+
+            
+        }
+
+        // Methods running at different speeds to show how tasks can be run simultaneously.
+        static void MethodA() {
+            WriteLine("Starting Method A...");
+            Thread.Sleep(3000); // simulate 3 seconds of work
+            WriteLine("Finished Method A.");
+        }static void MethodB() {
+            WriteLine("Starting Method B...");
+            Thread.Sleep(2000); // simulate 2 seconds of work
+            WriteLine("Finished Method B.");
+        }static void MethodC() {
+            WriteLine("Starting Method C...");
+            Thread.Sleep(1000); // simulate 1 seconds of work
+            WriteLine("Finished Method C.");
+        }
+    }
+}
