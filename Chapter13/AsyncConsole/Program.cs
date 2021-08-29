2bb0ec8fe0c8535846214defaed989642feb3845
Andrew Lam <lekz01@gmail.com>
2021-08-28 23:08:15 -0400

    Chapter 13 - Async/Await test on other sites

--- Chapter13/AsyncConsole/Program.cs
+++ Chapter13/AsyncConsole/Program.cs
@@ -6,10 +6,10 @@ using static System.Console;
 namespace AsyncConsole {
     class Program {
         // static void Main(string[] args) { // Will not work with async methods!
-        static async Task Main(string[] args) { // Allows Main() to work with async on C#7.1+
+        static async Task Main(string[] args) { // Allows Main() to work with async/await on C#7.1+
             HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync("http://www.apple.com/");
-            WriteLine("Apple's home page has {0:N0} bytes.",
+            HttpResponseMessage response = await client.GetAsync("http://www.crunchyroll.com/");
+            WriteLine("CrunchyRoll's home page has {0:N0} bytes.",
                 response.Content.Headers.ContentLength);
         }
     }
