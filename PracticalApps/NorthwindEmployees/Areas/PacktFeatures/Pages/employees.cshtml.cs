ef166cb167a512ef46302eb03ed0d016747ee995
Andrew Lam <lekz01@gmail.com>
2021-09-04 01:02:29 -0400

    Chapter 15 - Creating and using Razor class libraries for reusability

---
+++ PracticalApps/NorthwindEmployees/Areas/PacktFeatures/Pages/employees.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Packt.Shared;
+
+namespace PacktFeatures.Pages {
+
+    // Define a page model with an array of Employee entity instances loaded from the Northwind DB
+    public class EmployeesPageModel : PageModel {
+
+        private Northwind db;
+        public EmployeesPageModel(Northwind injectedContext) {
+            db = injectedContext;
+        }
+
+        public IEnumerable<Employee> Employees { get; set; }
+        public void OnGet()
+        {
+            Employees = db.Employees.ToArray();
+        }
+    }
+}
