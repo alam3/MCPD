e5ddbb72baebf1844e11ec5d666a1e75a3f88dbb
Andrew Lam <lekz01@gmail.com>
2021-09-06 16:30:37 -0400

    Chapter 15 - Building the simplest possible ASP.NET Core website project

---
+++ PracticalApps/SimpleWeb/Program.cs
@@ -0,0 +1,12 @@
+﻿// Building the simplest possible ASP.NET Core website project
+
+using Microsoft.AspNetCore.Hosting; // for IWebHostBuilder.Configure
+using Microsoft.AspNetCore.Builder; // for IApplicationBuilder.Run
+using Microsoft.AspNetCore.Http; // for HttpResponse.WriteAsync
+using Microsoft.Extensions.Hosting; // for Host
+
+Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => {
+    webBuilder.Configure(app => {
+        app.Run(context => context.Response.WriteAsync("Hello World Wide Web!"));
+    });
+}).Build().Run();
