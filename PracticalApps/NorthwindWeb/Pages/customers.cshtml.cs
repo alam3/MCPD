9337d3df951e3dfd0943477a74b2ebe69f1698d4
Andrew Lam <lekz01@gmail.com>
2021-09-07 00:09:06 -0400

    Chapter 15 - Exercise: Added page listing customers

---
+++ PracticalApps/NorthwindWeb/Pages/customers.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using Packt.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace NorthwindWeb.Pages {
+    public class CustomersModel : PageModel {
+        private Northwind db;
+        public CustomersModel(Northwind injectedContext) {
+            db = injectedContext;
+        }
+
+        public IEnumerable<SortedList<string,string>> Customers { get; set; }
+
+        public void OnGet() {
+            ViewData["Title"] = "Northwind Web Site - Customers";
+
+            Customers = db.Customers.Select(c => new SortedList<string,string>() {
+                {"CustomerName", c.CompanyName},
+                {"Country", c.Country}
+            });
+        }
+    }
+}
