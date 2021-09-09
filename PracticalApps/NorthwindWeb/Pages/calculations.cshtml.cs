74b6c31ef9b57281fa38fc50b1d0e5c4138d7ced
Andrew Lam <lekz01@gmail.com>
2021-09-08 23:23:34 -0400

    Chapter 15 - Exercise 15.3: Generate factorial and fibonacci sequence with n terms

--- PracticalApps/NorthwindWeb/Pages/calculations.cshtml.cs
+++ PracticalApps/NorthwindWeb/Pages/calculations.cshtml.cs
@@ -5,29 +5,61 @@ using Packt.Shared;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using static System.Console;
+using System.Numerics;
 
 
 namespace NorthwindWeb.Pages {
     public class CalculationsModel : PageModel {
-        public List<int> Result { get; set; }
-        
+        public List<long> TimesTableResult { get; set; }
+        public BigInteger FactorialResult { get; set; }
+        List<long> fibonacciResult;
+        public string FibonacciSequenceString { get; set; }
+
         [BindProperty]
-        public int Multiplicand { get; set; }
+        public long Multiplicand { get; set; }
+
+        public CalculationsModel() {
+            TimesTableResult = new List<long>();
+            fibonacciResult = new List<long>();
+            Multiplicand = 0;
+        }
 
         public void OnGet() {
-            Multiplicand = 0;
-            Result = new List<int>() { 0 };
         }
 
         public IActionResult OnPost() {
             WriteLine($"Post value: {Multiplicand}");
-            Result = new List<int>();
             if (ModelState.IsValid) {
                 for (int i = 1; i <= 12; i++) {
-                    Result.Add(Multiplicand * i);
+                    TimesTableResult.Add(Multiplicand * i);
                 }
+
+                FactorialResult = Factorial(Multiplicand);
+
+                for (long i = 1; i <= Multiplicand; i++) {
+                    fibonacciResult.Add(FibonacciTerm(i));
+                }
+                FibonacciSequenceString = string.Join(", ", fibonacciResult.ToArray());
             }
             return Page();
         }
+
+        BigInteger Factorial(long inputNumber) {
+            if (inputNumber < 1) {
+                return 0;
+            } else if (inputNumber == 1) {
+                return 1;
+            } else {
+                checked {
+                    return inputNumber * Factorial(inputNumber - 1);
+                }
+            }
+        }
+
+        long FibonacciTerm(long term) => term switch {
+            1 => 0,
+            2 => 1,
+            _ => FibonacciTerm(term - 1) + FibonacciTerm(term - 2)
+        };
     }
 }
