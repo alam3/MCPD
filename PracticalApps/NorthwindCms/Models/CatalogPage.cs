065a390c869fe34650a3ca9d76664892931b1ae3
Andrew Lam <lekz01@gmail.com>
2021-10-04 15:53:20 -0400

    Chapter 17 - Piranha CMS - Defining Custom Page Types for the Catalog Page entry point and Category Pages.

---
+++ PracticalApps/NorthwindCms/Models/CatalogPage.cs
@@ -0,0 +1,11 @@
+// Custom page type for the Catalog page. Acts as an entry point and container for category pages.
+
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace NorthwindCms.Models {
+    [PageType(Title = "Catalog page", UseBlocks = false)] // disable blocks use
+    [PageTypeRoute(Title = "Default", Route = "/catalog")] // define custom route path
+    public class CatalogPage : Page<CatalogPage> {
+    }
+}
---
+++ PracticalApps/NorthwindCms/Models/CategoryPage.cs
@@ -0,0 +1,20 @@
+// Custom page type for product category pages.
+
+using Piranha.AttributeBuilder;
+using Piranha.Extend;
+using Piranha.Models;
+using System.Collections.Generic;
+
+namespace NorthwindCms.Models {
+    [PageType(Title = "Category Page", UseBlocks = false)] // Do not allow blocks use
+    [PageTypeRoute(Title = "Default", Route = "/catalog-category")] // define custom route
+    public class CategoryPage : Page<CategoryPage> {
+        [Region(Title = "Category detail")]
+        [RegionDescription("The details for this category.")]
+        public CategoryRegion CategoryDetail { get; set; }
+
+        [Region(Title = "Category products")]
+        [RegionDescription("The products for this category.")]
+        public IList<ProductRegion> Products { get; set; } = new List<ProductRegion>();
+    }
+}
