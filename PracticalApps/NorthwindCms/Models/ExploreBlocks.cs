afb486e4b63f4b3757ed0675fc2661cec393e23e
Andrew Lam <lekz01@gmail.com>
2021-10-04 13:51:02 -0400

    Chapter 17 - Piranha CMS - Exploring the block methods.

---
+++ PracticalApps/NorthwindCms/Models/ExploreBlocks.cs
@@ -0,0 +1,14 @@
+// using Piranha.Extend.Blocks;
+
+// class ExploreBlocks {
+//     AudioBlock ab;
+//     HtmlBlock hb;
+//     ColumnBlock cb;
+//     ImageBlock ib;
+//     ImageGalleryBlock igb;
+//     PageBlock pb;
+//     QuoteBlock qb;
+//     SeparatorBlock sb;
+//     TextBlock tb;
+//     VideoBlock vb;
+// }
