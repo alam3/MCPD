3b76c596c0ad5c9f094148845c34f9d85631d36c
Andrew Lam <lekz01@gmail.com>
2021-10-04 15:11:18 -0400

    Chapter 17 - Piranha CMS - Defining Custom Regions to store information about a category from the Northwind DB in the Piranha CMS.

---
+++ PracticalApps/NorthwindCms/Models/CategoryRegion.cs
@@ -0,0 +1,17 @@
+// Defines a region for storing information about a category from the Northwind database
+
+using Piranha.Extend;
+using Piranha.Extend.Fields;
+
+namespace NorthwindCms.Models {
+    public class CategoryRegion {
+        [Field(Title = "Category ID")]
+        public NumberField CategoryID { get; set; }
+        [Field(Title = "Category name")]
+        public TextField CategoryName { get; set; }
+        [Field]
+        public HtmlField Description { get; set; }
+        [Field(Title = "Category image")]
+        public ImageField CategoryImage { get; set; }
+    }
+}
---
+++ PracticalApps/NorthwindCms/Models/ProductRegion.cs
@@ -0,0 +1,18 @@
+// Defines a region for storing information about a product from the Northwind database
+
+using Piranha.Extend;
+using Piranha.Extend.Fields;
+using Piranha.Models;
+
+namespace NorthwindCms.Models {
+    public class ProductRegion {
+        [Field(Title = "Product ID")]
+        public NumberField ProductID { get; set; }
+        [Field(Title = "Product name")]
+        public TextField ProductName { get; set; }
+        [Field(Title = "Unit price", Options = FieldOption.HalfWidth)]
+        public StringField UnitPrice { get; set; }
+        [Field(Title = "Units in stock", Options = FieldOption.HalfWidth)]
+        public NumberField UnitsInStock { get; set; }
+    }
+}
