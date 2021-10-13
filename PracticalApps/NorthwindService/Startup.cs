e01c190620f5adc57d44d6b6084ba0b8591be540
Andrew Lam <lekz01@gmail.com>
2021-10-12 23:06:29 -0400

    Chapter 18 - Web Services - Creating a web service for Northwind DB. Display default formatter data and add XML formatter.

--- PracticalApps/NorthwindService/Startup.cs
+++ PracticalApps/NorthwindService/Startup.cs
@@ -11,6 +11,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+// Creating a web service for the Northwind Database
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Packt.Shared;
+using static System.Console;
 
 namespace NorthwindService
 {
@@ -26,8 +32,28 @@ namespace NorthwindService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configuring the Northwind data context
+            string databasePath = Path.Combine("..", "Northwind.db");
+            services.AddDbContext<Northwind>(options => options.UseSqlite($"Data Source={databasePath}"));
 
-            services.AddControllers();
+            // show supported media types of default output formatters, add XML serializer formatters, and set
+            // compatibility to ASP.NET Core 3.0
+            services.AddControllers(options => {
+                WriteLine("Default output formatters:");
+                foreach(IOutputFormatter formatter in options.OutputFormatters) {
+                    var mediaFormatter = formatter as OutputFormatter;
+                    if (mediaFormatter == null) {
+                        WriteLine($" {formatter.GetType().Name}");
+                    } else { // OutputFormatter class has SupportedMediaTypes
+                        WriteLine(" {0}, Media types: {1}",
+                            arg0: mediaFormatter.GetType().Name,
+                            arg1: string.Join(", ", mediaFormatter.SupportedMediaTypes));
+                    }
+                }
+            }).AddXmlDataContractSerializerFormatters()
+            .AddXmlSerializerFormatters()
+            .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "NorthwindService", Version = "v1" });
